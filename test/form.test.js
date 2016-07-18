12f798fdea8e1b640d3f6f7ca2d80b4437bd3bc4
Ian Lenehan <ianlenehan@gmail.com>
2016-07-18 12:37:41 +1000

    first commit, test suite added

---
+++ test/form.test.js
@@ -0,0 +1,10 @@
+var expect = require("chai").expect;
+var form = require("../app/form");
+
+describe("Basic Test", function () {
+  describe("Adding 1 + 1", function () {
+    it("should equal 2", function () {
+      expect(1+1).to.equal(2);
+    });
+  });
+});
